fe5d03cdbd3bf2c131a5b2080acdf973f59b2782
jarif010 <jarifhaider010@gmail.com>
2025-01-03 17:02:03 +0600

    Changing Colors Randomly

---
+++ Changing colours/script.js
@@ -0,0 +1,15 @@
+function orange(){
+    document.body.className="orange"
+}
+function blue(){
+    document.body.className="blue"
+}
+function green(){
+    document.body.className="green"
+}
+function pink(){
+    document.body.className="pink"
+}
+function red(){
+    document.body.className="red"
+}
